9e483afff90ef5d4ae2326cda3a1a5468aa87b88
Kacper Miklewicz <s24286@pjwstk.edu.pl>
2022-11-26 19:19:36 +0100

    added new test

--- src/test/java/pl/pjatk/kmiklewicz/CarServiceTests.java
+++ src/test/java/pl/pjatk/kmiklewicz/CarServiceTests.java
@@ -49,11 +49,29 @@ public class CarServiceTests {
         LocalDate startDate = LocalDate.of(2022,11,25);
         LocalDate endDate = LocalDate.of(2022,11,30);
 
-        // When
+        RentalInfo rentalInfo = carService.rentCar(user, vin, startDate, endDate);
 
+        // When
+        double price = rentalInfo.getPrice();
 
         // Then
+        assertThat(price).isEqualTo(50);
+    }
 
+    @Test
+    void shouldReturnGoodPriceWhenCarTypeStandard() {
+        // Given
+        User user = new User("1");
+        int vin = 5;
+        LocalDate startDate = LocalDate.of(2022,11,25);
+        LocalDate endDate = LocalDate.of(2022,11,30);
 
+        RentalInfo rentalInfo = carService.rentCar(user, vin, startDate, endDate);
+
+        // When
+        double price = rentalInfo.getPrice();
+
+        // Then
+        assertThat(price).isEqualTo(25);
     }
 }
